15f8036e39f5c730b8e105f616ac0fe7f98919cd
Ruben Ollesch <ruben.ollesch@protonmail.com>
2018-09-06 10:10:33 +0200

    spNewCompany

---
+++ Database1/StoredProcedures/spNewCompany.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[spNewCompany]
+	@companyName varchar(256)
+AS
+	INSERT INTO Company (CompanyName)
+	VALUES (@companyName)
+RETURN 0
--- Database1/Table/Company.sql
+++ Database1/Table/Company.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Company]
 (
-	[Id] INT NOT NULL PRIMARY KEY,
+	[Id] INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	[CompanyName] VARCHAR(256) NOT NULL,
 	[CreationTime] DATETIME NOT NULL DEFAULT getDate(),
 	[DeletionTime] DATETIME,
