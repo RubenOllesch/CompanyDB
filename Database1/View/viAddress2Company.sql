45d0920645ed483877a4ebf1dda04f5c455d0289
Ruben Ollesch <ruben.ollesch@protonmail.com>
2018-09-07 08:56:10 +0200

    Improved View for A2C

--- Database1/View/viAddress2Company.sql
+++ Database1/View/viAddress2Company.sql
@@ -1,2 +1,5 @@
 ﻿CREATE VIEW [dbo].[viAddress2Company]
-	AS SELECT AddressId, CompanyId FROM Address2Company
+	AS SELECT A2C.CompanyId, C.CompanyName, A2C.AddressId
+	FROM [Company] C
+	INNER JOIN [Address2Company] A2C ON C.Id = A2C.CompanyId 
+	INNER JOIN [Address] A ON a2c.AddressId = a.Id
